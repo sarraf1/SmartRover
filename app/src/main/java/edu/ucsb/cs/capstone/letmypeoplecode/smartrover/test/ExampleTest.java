18072a8e927be8cd95f51fffb98f6dab2584154a
Chris Nelson <sciencectn@gmail.com>
2014-02-16 15:21:37 -0800

    Placed the setup instructions in a comment

--- app/src/main/java/edu/ucsb/cs/capstone/letmypeoplecode/smartrover/test/ExampleTest.java
+++ app/src/main/java/edu/ucsb/cs/capstone/letmypeoplecode/smartrover/test/ExampleTest.java
@@ -2,9 +2,18 @@ package edu.ucsb.cs.capstone.letmypeoplecode.smartrover.test;
 
 import android.test.InstrumentationTestCase;
 
-/**
- * Created by Chris on 2/16/14.
+/*
+Steps to make this work:
+1. Run -> Edit Configurations
+2. Add an "Android Tests" config, name it whatever
+3. Choose "All in package", set package to
+edu.ucsb.cs.capstone.letmypeoplecode.smartrover.test
+4. Add a filter in logcat to catch messages with "TestRunner" if you
+want to see the test output
+5. When you run it, Event Log should say Tests failed
  */
+
+
 public class ExampleTest extends InstrumentationTestCase {
     public void test() throws Exception{
         final int expected=1;
